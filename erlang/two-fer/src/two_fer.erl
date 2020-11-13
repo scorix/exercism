b71594e2aa980c2bcbbde5ad3ab71a08af492699
scorix <scorix@gmail.com>
2020-11-13 17:03:55 +0800

    finish two_fer on erlang track

--- erlang/two-fer/src/two_fer.erl
+++ erlang/two-fer/src/two_fer.erl
@@ -2,7 +2,6 @@
 
 -export([two_fer/0, two_fer/1]).
 
+two_fer() -> two_fer("you").
 
-two_fer() -> undefined.
-
-two_fer(_Name) -> undefined.
+two_fer(Name) -> io_lib:format("One for ~s, one for me.", [Name]).
