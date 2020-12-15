dd3e0c8f049e70e9f4a0a0226cf0f6ba7ad92f76
scorix <scorix@gmail.com>
2020-12-15 20:14:44 +0800

    finish anagram on rust track

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,9 +1,33 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{}' word find anagrams among the following words: {:?}",
-        word,
-        possible_anagrams
-    );
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&str]) -> HashSet<&'a str> {
+    possible_anagrams
+        .iter()
+        .cloned()
+        .filter(|a| anagram_for(word, a))
+        .collect()
+}
+
+fn anagram_for(word: &str, anagram: &str) -> bool {
+    if word.len() != anagram.len() {
+        return false;
+    }
+
+    let mut wcs = word
+        .chars()
+        .map(|a| a.to_lowercase().to_string())
+        .collect::<Vec<_>>();
+    let mut ncs = anagram
+        .chars()
+        .map(|a| a.to_lowercase().to_string())
+        .collect::<Vec<_>>();
+
+    if wcs == ncs {
+        return false;
+    }
+
+    wcs.sort();
+    ncs.sort();
+
+    wcs == ncs
 }
