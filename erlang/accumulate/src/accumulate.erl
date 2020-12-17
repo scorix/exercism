333304a642a43fdc9854c2e681bd6c8c4afb8848
scorix <scorix@gmail.com>
2020-12-17 10:52:10 +0800

    transform accumulate into tail recursive on erlang track

--- erlang/accumulate/src/accumulate.erl
+++ erlang/accumulate/src/accumulate.erl
@@ -2,6 +2,10 @@
 
 -export([accumulate/2]).
 
-accumulate(_, []) -> [];
-accumulate(Fn, [Head|Tail]) ->
-  [Fn(Head) | accumulate(Fn, Tail)].
+accumulate(Fn, Ls) ->
+  tail_accumulate(Fn, [], Ls).
+
+tail_accumulate(_, HeadList, []) ->
+  HeadList;
+tail_accumulate(Fn, HeadList, [Head|Tail]) ->
+  tail_accumulate(Fn, HeadList++[Fn(Head)], Tail).
