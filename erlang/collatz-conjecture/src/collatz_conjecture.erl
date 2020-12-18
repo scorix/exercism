3479dbf5740c9dfc965201aebbb1a9a398879c15
scorix <scorix@gmail.com>
2020-12-18 10:56:21 +0800

    finish collatz-conjecture on erlang track

--- erlang/collatz-conjecture/src/collatz_conjecture.erl
+++ erlang/collatz-conjecture/src/collatz_conjecture.erl
@@ -3,4 +3,14 @@
 -export([steps/1]).
 
 
-steps(_N) -> undefined.
+steps(N) ->
+    count_steps(0, N).
+
+count_steps(_, N) when N =< 0 ->
+    erlang:error(badarg);
+count_steps(S, 1) ->
+    S;
+count_steps(S, N) when N rem 2 == 0 ->
+    count_steps(S+1, N div 2);
+count_steps(S, N) ->
+    count_steps(S+1, N*3+1).
