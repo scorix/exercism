35cb7316213a2623d503ab5ab5ae9124b09f7a36
scorix <scorix@gmail.com>
2020-12-21 14:09:57 +0800

    finish raildrops on erlang track

--- erlang/raindrops/src/raindrops.erl
+++ erlang/raindrops/src/raindrops.erl
@@ -3,4 +3,10 @@
 -export([convert/1]).
 
 
-convert(_Number) -> undefined.
+convert(Number) -> convert_and_concat(Number, 0, []).
+
+convert_and_concat(Number, Factor, Sound) when Number rem 3 == 0, Factor =/= 3 -> convert_and_concat(Number div 3, 3, lists:concat([Sound, "Pling"]));
+convert_and_concat(Number, Factor, Sound) when Number rem 5 == 0, Factor =/= 5 -> convert_and_concat(Number div 5, 5, lists:concat([Sound, "Plang"]));
+convert_and_concat(Number, Factor, Sound) when Number rem 7 == 0, Factor =/= 7 -> convert_and_concat(Number div 7, 7, lists:concat([Sound, "Plong"]));
+convert_and_concat(Number, _, []) -> lists:concat([Number]);
+convert_and_concat(_, _, Sound) -> Sound.
