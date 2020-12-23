0825272c3abfe55162253f8da9275a6e6f8afa4e
scorix <scorix@gmail.com>
2020-12-23 10:49:30 +0800

    use == instead of compare_equal

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -7,42 +7,31 @@ pub enum Comparison {
 }
 
 pub fn sublist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison {
-    if first_list.len() == second_list.len() {
-        return compare_equal(first_list, second_list);
-    }
-
-    if first_list.len() > second_list.len() {
-        if compare_sublist(second_list, first_list) == Comparison::Sublist {
-            return Comparison::Superlist;
-        }
-    }
-
-    compare_sublist(first_list, second_list)
-}
-
-fn compare_equal<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison {
-    if first_list.len() == 0 {
+    if first_list == second_list {
         return Comparison::Equal;
     }
 
-    for idx in 0..first_list.len() {
-        if first_list[idx] != second_list[idx] {
-            return Comparison::Unequal;
-        }
-    }
-
-    Comparison::Equal
+    compare(first_list, second_list)
 }
 
-fn compare_sublist<T: PartialEq>(shorter_list: &[T], longer_list: &[T]) -> Comparison {
-    if shorter_list.len() == 0 {
-        return Comparison::Sublist;
+fn compare<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison {
+    let mut res = Comparison::Superlist;
+    let mut longer_list = first_list;
+    let mut shorter_list = second_list;
+
+    if first_list.len() < second_list.len() {
+        res = Comparison::Sublist;
+        longer_list = second_list;
+        shorter_list = first_list;
     }
 
-    for list in longer_list.windows(shorter_list.len()) {
-        match compare_equal(list, shorter_list) {
-            Comparison::Equal => return Comparison::Sublist,
-            _ => continue
+    if shorter_list.len() == 0 {
+        return res;
+    }
+
+    for sublist in longer_list.windows(shorter_list.len()) {
+        if sublist == shorter_list {
+            return res
         }
     }
 
