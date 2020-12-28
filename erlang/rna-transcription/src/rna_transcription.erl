c3dcec1e59f87f7fe14fa26255d773c506236560
scorix <scorix@gmail.com>
2020-12-28 11:45:52 +0800

    finish rna-transcription on erlang track

--- erlang/rna-transcription/src/rna_transcription.erl
+++ erlang/rna-transcription/src/rna_transcription.erl
@@ -2,5 +2,14 @@
 
 -export([to_rna/1]).
 
+to_rna([]) -> [];
+to_rna(String) -> transform(String, []).
 
-to_rna(_Strand) -> undefined.
+transform([H|T], L) -> transform(T, L++rna([H]));
+transform([], L) -> L.
+
+rna("C") -> "G";
+rna("G") -> "C";
+rna("T") -> "A";
+rna("A") -> "U";
+rna(_) -> "".
